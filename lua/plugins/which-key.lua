cacd791403833b0762370a0088b140ebe3f493b9
Jack Wenyoung <dlwxxxdlw@gmail.com>
2025-02-07 10:50:05 +0800

    Setup some basic keymaps

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -6,7 +6,16 @@ return {
       vim.cmd.packadd("which-key")
   end,
   after = function ()
-    require("which-key").setup({})
+    local wk = require("which-key")
+    wk.add({
+      { "<leader>s", group="Search" },
+      { "<leader>sf", "<cmd>FzfLua files<cr>", desc = "Find Files", mode = "n" },
+      { "<leader>sp", "<cmd>FzfLua live_grep_resume<cr>", desc = "Live Grep Project", mode = "n" },
+      { "<leader>sb", "<cmd>FzfLua lgrep_curbuf<cr>", desc = "Live Grep CurBuf", mode = "n" },
+      { "<leader>sw", "<cmd>FzfLua cword<cr>", desc = "Search Word", mode = "n" },
+      { "<leader>b", group="Buffer" },
+      { "<leader>bb", "<cmd>FzfLua buffers<cr>", desc = "Select Buffer", mode = "n" },
+    })
   end
 }
 
